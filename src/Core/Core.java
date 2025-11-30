5609a36c9c5321c854bb680835a47281f3986e02
msshazil <malikshazilms04@gmial.com>
2025-11-30 23:30:48 +0500

    changing places

--- src/Core/Core.java
+++
@@ -1,6 +0,0 @@
-package Core;
-
-public class Core {
-    private String msg;
-    
-}
--- src/com/dronesimulator/Main.java
+++ src/com/dronesimulator/Main.java
@@ -1,7 +1,13 @@
 package com.dronesimulator;
 
+import Control.Simulator;
+import Core.Vector3;
+import Physics.Drone;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Drone Simulator Project Initialized!");
-    }
+public static void main(String[] args) {
+    Simulator sim = new Simulator();
+    sim.drones.add(new Drone(0, new Vector3(0,0,1), 1.0));
+    sim.run();
+}
 }
