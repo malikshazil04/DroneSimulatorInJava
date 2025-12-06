9ba61ce93b6e2d430faf4e07f5d548fc2c3a9e2a
msshazil <malikshazilms04@gmial.com>
2025-12-06 23:33:05 +0500

    Formation main

--- src/com/dronesimulator/Main.java
+++ src/com/dronesimulator/Main.java
@@ -4,18 +4,28 @@ import Core.Simulator;
 import Core.Vector3;
 import Physics.Drone;
 import Control.Controller;
+import Control.FormationManager;
 
 public class Main {
 public static void main(String[] args) {
 
-    Controller controller = new Controller();
-    Simulator sim = new Simulator(controller);
+Controller controller = new Controller();
+Simulator sim = new Simulator(controller);
 
-    Drone d1 = new Drone();
-    d1.setTarget(new Vector3(10, 10, 10));
-    sim.addDrone(d1);
+FormationManager formation = sim.getFormationManager();
+formation.setSpacing(4.0);
 
-    sim.run();
+Drone d1 = new Drone();
+d1.setPosition(new Vector3(0, 0, 0));
+d1.setTarget(new Vector3(10, 0, 5));
+
+Drone d2 = new Drone();
+d2.setPosition(new Vector3(2, 0, 0));
+d2.setTarget(new Vector3(10, 2, 5));
+
+sim.addDrone(d1);
+sim.addDrone(d2);
+sim.run();
 }
 }
 
